b9893450589c43bc1fe82c58726be1b86a419cf7
shivam-181 <149074738+shivam-181@users.noreply.github.com>
2025-10-12 15:51:34 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (6.14%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string filtered="";
-        for(char ch:s)
-        {
-            if(isalnum(ch))
-            filtered+=tolower(ch);
+        string filtered = "";
+        for (char ch : s) {
+            if (isalnum(ch))
+                filtered += tolower(ch);
         }
-        int st=0;
-        int e=filtered.length()-1;
-        while(st<=e)
-        {
-            if(filtered[st]!=filtered[e])
-            return false;
-            st++;
-            e--;
-        }
-        return true;
-
+        string s2 = filtered;
+        reverse(s2.begin(), s2.end());
+       return filtered==s2;
     }
 };
