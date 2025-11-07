225c32ce76e93c4b6bcc2d68233b167b4488765b
Shivam Kumar <149074738+shivam-181@users.noreply.github.com>
2025-11-07 22:31:09 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (55.63%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,34 @@
+class Solution {
+private:
+    void solve(string digits,string output,int index,vector<string>& ans,string mapping[])
+    {
+        //  base case
+        if(index>=digits.length())
+        {
+            ans.push_back(output);
+            return;
+        }
+        // to store in string
+        int number=digits[index]-'0';
+        string value=mapping[number];
+        // this loop is for resp. alphabets from mapping values
+        for(int i=0;i<value.length();i++)
+        {
+            output.push_back(value[i]);
+            solve(digits,output,index+1,ans,mapping);
+            output.pop_back();
+        }
+    }
+public:
+    vector<string> letterCombinations(string digits) {
+        vector<string> ans;
+        if(digits.length()==0)
+        return ans;
+        string output;
+        int index=0;
+        // From 0 to 9
+        string mapping[10]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        solve(digits,output,index,ans,mapping);
+        return ans;
+    }
+};
