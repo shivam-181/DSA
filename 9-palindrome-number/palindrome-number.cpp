d4c3b8080fe6a236878ba384ebe29c796e07bf97
Shivam Kumar <149074738+shivam-181@users.noreply.github.com>
2025-12-01 21:47:42 +0530

    Time: 4 ms (32.89%) | Memory: 8.6 MB (42.31%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;  
+        long long rev = 0, num = x;
+        while(num) {
+            rev = rev * 10 + num % 10;  
+            num /= 10;
+        }
+        return rev == x;  
+    }
+};
